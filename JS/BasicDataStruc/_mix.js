a415f8cb15dc27aed0c2c15755fc155e1d9b0dd7
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-07-26 13:07:45 +0200

    added new dir for new chapter 'Basic Data Structures', added mix file to it

---
+++ JS/BasicDataStruc/_mix.js
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------------------
+// Check For The Presence of an Element With indexOf()
+// -----------------------------------------------------------------------------------------
+
+function quickCheck( arr, elem ) {
+  // change code below this line
+  
+  return arr.indexOf( elem ) !== -1;
+  
+  // change code above this line
+}
+
+// change code here to test different cases:
+console.log( quickCheck( ['squash', 'onions', 'shallots'], 'mushrooms' ) );
