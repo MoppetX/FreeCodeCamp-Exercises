9a46031347d923a68c624d224b51f6b706a0505c
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-08-01 11:28:36 +0200

    finished 'Falsy Bouncer' challenge

--- JS/basicAlgorithmScript/_mix.js
+++ JS/basicAlgorithmScript/_mix.js
@@ -1,19 +1,35 @@
+// ----------------------------------------------------------------------------------
 // L E S S O N:
-function findElement(arr, func) {
+function findElement( arr, func ) {
   
   return arr.filter( func )[0];
 }
 
-findElement([1, 3, 5], num => num % 2 === 0);
+findElement( [1, 3, 5], num => num % 2 === 0 );
 
-
-
+// ----------------------------------------------------------------------------------
 // L E S S O N:  Boo who
-function booWho(bool) {
-  // What is the new fad diet for ghost developers? The Boolean.
+function booWho( bool ) {
   // return bool === true ? true : bool === false ? true : false;
   return typeof bool === 'boolean';
 }
 
-console.log(booWho(false));
+console.log( booWho( false ) );
 
+// ----------------------------------------------------------------------------------
+// L E S S O N:  Falsy Bouncer
+function bouncer( arr ) {
+  // Remove all falsy values from an array.
+  return arr.filter( elem => elem );
+}
+
+console.log( bouncer( [0, 7, 'ate', '', false, 9] ) );
+
+// F C C  S O L U T I O N
+//
+// function bouncer(arr) {
+//   return arr.filter(Boolean);
+// }
+// The Array.prototype.filter method expects a function that returns a Boolean
+// value which takes a single argument and returns true for truthy value or false
+// for falsy value. Hence we pass the built-in Boolean function.
