95b9504a0a7959b6b39779279bb252d31c102ec1
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-08-19 13:11:39 +0200

    finished first interm. Algorithm Scripting challenge

---
+++ JS/intermAlgorithmScript/diffTwoArrays.js
@@ -0,0 +1,25 @@
+// Compare two arrays and return a new array with any items only found in one of the two given arrays,
+// but not both. In other words, return the symmetric difference of the two arrays.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// NOTE: You can return the array with its elements in any order.
+
+// M Y  S O L U T I O N
+function diffArray( arr1, arr2 ) {
+  var newArr = [];
+  // Same, same; but different.
+  return newArr;
+}
+
+diffArray( [1, 2, 3, 5], [1, 2, 3, 4, 5] );
+
+//
+// F C C  S O L U T I O N S
+// B A S I C
+
+//
+// I N T E R M E D I A T E  Solution
+
+//
+// A D V A N C E D Solution
